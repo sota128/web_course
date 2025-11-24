cac81bdbd5f2ca9edf70a8b40762cd08b6f5b557
sota128 <176373118+sota128@users.noreply.github.com>
2025-11-24 23:02:55 +0900

    ex3の修正

--- 課題3/ex3_lib.js
+++ 課題3/ex3_lib.js
@@ -1,3 +1,4 @@
+// 配列の合計値を計算して返す関数
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 function sub_elem(array) {
     let total = 0;
@@ -6,5 +7,27 @@ function sub_elem(array) {
     }
     return total;
 }
+// 関数を呼び出し、結果をページに表示
 let total = sub_elem(array);
-document.write(total);
+document.write('配列の合計値: ' + total + '<br>');
+
+
+// personオブジェクトを作成
+const person = {
+    name: '山内颯大',
+    grade: 3,
+    class: '情報科学・工学系'
+};
+
+// オブジェクトの情報をコンソールに表示する関数
+function displayPersonInfo(p) {
+    console.log('名前: ' + p.name);
+    console.log('学年: ' + p.grade);
+    console.log('系: ' + p.class);
+}
+
+// 関数を呼び出し、情報をコンソールに表示
+displayPersonInfo(person);
+
+// コンソールを確認するようメッセージを表示
+document.write('personオブジェクトの情報はコンソールを確認してください。');
